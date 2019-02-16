7eacc069a0a7fb85c460df6dcbb2fd43a530853e
Shahed Iqbal <shahediqbal13@gmail.com>
2019-02-16 13:42:30 +0600

    Spring mvc starter project

---
+++ spring-mvc-starter/src/main/java/com/shahediqbal/springmvcstarter/SpringMvcStarterApplication.java
@@ -0,0 +1,14 @@
+package com.shahediqbal.springmvcstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcStarterApplication.class, args);
+    }
+
+}
+
